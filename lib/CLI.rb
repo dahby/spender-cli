f4503a792ad16fd7164e0b16afb57ee4bab0e890
dahby <dstoll243@gmail.com>
2019-05-28 18:50:16 -0700

    added basic functionality for logging in and signing up

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -5,24 +5,84 @@ class CLI
   end
 
   def run
+    system("clear")
     puts "WELCOME TO SPENDER!"
     puts
     welcome_menu
   end
 
   def welcome_menu
+    is_running = true
+    while is_running
+      print_welcome
+      binding.pry
+      selection = STDIN.gets.chomp.to_i
+      if selection == 1
+        login_menu
+      elsif selection == 2
+        system("clear")
+        sign_up_menu
+        # TODO: build out sign-up menu
+      elsif selection == 3
+        # TODO: build out about section
+      elsif selection == 9
+        puts "THANK YOU!!!"
+        puts
+        is_running = false
+      else
+        puts "Please input a valid selection"
+      end
+    end
+  end
+
+  def print_welcome
     puts "Please make a selection:"
     puts "1 - Login"
-    puts "2 - About Spender"
+    puts "2 - Sign Up"
+    puts "3 - About Spender"
+    puts "9 - Exit Application"
     puts
-    selection = STDIN.gets.chomp.to_i
-    if selection != 1 && selection != 2
-      puts "Please input a valid selection"
-      welcome_menu
-    elsif selection == 1
-      puts 'LOGIN MENU'
-    elsif selection == 2
-      puts 'ABOUT MENU'
+    print "Your Selection: " 
+  end
+
+  def login_menu
+    system("clear")
+    puts "LOGIN MENU"
+    puts
+    print "Username: "
+    username = STDIN.gets.chomp
+    current_user = User.find_by(username: username)
+    if current_user
+      print "Enter your PIN: "
+      pin = STDIN.gets.chomp.to_i
+      if current_user.pin == pin
+        @current_user = current_user
+      else
+        puts "ERROR: Invalid Pin"
+        login_menu
+      end
+    else
+      puts "No Such User"
     end
+  end
+
+  def sign_up_menu
+    puts "SIGN UP MENU"
+    puts
+    print "Enter a Username: "
+    username = STDIN.gets.chomp
+    current_user = User.find_by(username: username)
+    if current_user
+      puts "Username already exists, pick again."
+      return sign_up_menu
+    end
+    puts "Choose a PIN: "
+    pin = STDIN.gets.chomp.to_i
+    user = User.create(username: username, pin: pin)
+    @current_user = user
+  end
+
+  def set_current_user(user)
+    @current_user = user
   end
 end
