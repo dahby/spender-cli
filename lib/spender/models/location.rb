d62ef735f8202144b61796634e8c27e1d00f098f
dahby <dstoll243@gmail.com>
2019-06-01 15:21:37 -0700

    total based on location name method fun tioning

--- lib/spender/models/location.rb
+++ lib/spender/models/location.rb
@@ -1,4 +1,12 @@
 class Location < ActiveRecord::Base
   has_many :transactions
   has_many :users, through: :transactions
+
+  def update_name(new_name)
+    self.update(name: new_name.capitalize)
+  end
+
+  def update_category(new_category)
+    self.update(category: new_category.downcase)
+  end
 end
