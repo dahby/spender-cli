c7d6bc4e4b3678eb88acd5d82e94ceaac7822478
dahby <dstoll243@gmail.com>
2019-06-01 15:01:02 -0700

    Modify transaction location (update or create new)

--- lib/spender/models/transaction.rb
+++ lib/spender/models/transaction.rb
@@ -5,4 +5,8 @@ class Transaction < ActiveRecord::Base
   def update_price(new_price)
     self.update(price: new_price)
   end
+
+  def update_location(new_location)
+    self.update(location: new_location)
+  end
 end
