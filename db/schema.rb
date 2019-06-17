aa9d3ec858e37ab74cbc3e87f3b6455a371c8719
dahby <dstoll243@gmail.com>
2019-06-17 16:10:19 -0700

    added purchase_date column to transaction, migrated successfully

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,3 @@
-# This file is part of Spender.
-
-# Spender is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Spender is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Spender.  If not, see <https://www.gnu.org/licenses/>.
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -25,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_180926) do
+ActiveRecord::Schema.define(version: 2019_06_17_230823) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -40,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_01_180926) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "purchase_date"
   end
 
   create_table "users", force: :cascade do |t|
